0fa8a773080811b307e80899a6eb0ab6f9632a8a
Ianne <bulilaniannecarl@gmail.com>
2025-07-05 21:37:21 +0800

    initiate repo

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bubbleGold: "#F6D776", // pastel gold
+      },
+      keyframes: {
+        float: {
+          "0%": { transform: "translateY(0px)" },
+          "100%": { transform: "translateY(-10px)" },
+        },
+      },
+      animation: {
+        float: "float var(--duration) ease-in-out var(--delay) infinite alternate",
+      },
+    },
+  },
+  plugins: [],
+}
+
