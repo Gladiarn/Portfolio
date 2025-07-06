e80ec919650212e9ee655a1cccea12d5045b4114
Ianne <bulilaniannecarl@gmail.com>
2025-07-06 16:40:37 +0800

    create background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,34 @@
-
+import BubbleBackground from "@/components/Background/BubbleBackground";
+import bubbleTypes from "@/components/types";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [bubbles, setBubbles] = useState<bubbleTypes[]>([]);
+
+  useEffect(() => {
+    const total = 200;
+    const list = [];
+
+    for (let i = 0; i < total; i++) {
+      list.push({
+        id: i,
+        size: Math.floor(Math.random() * 4) + 1,
+        left: `${Math.random() * 100}vw`,
+        top: `${Math.random() * 100}vh`,
+        duration: `${2 + Math.random() * 3}s`,
+        delay: `${Math.random() * 5}s`,
+      });
+
+      setBubbles(list);
+      console.log(list);
+    }
+  }, []);
+
   return (
     <div className="w-full h-screen bg-[#121212]">
+{/* Background */}
+    <BubbleBackground bubbles={bubbles} />
+
     </div>
   );
 }
