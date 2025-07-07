a7c2e0ac9176ee0d0ba8007a58c11a98a2708e15
Ianne <bulilaniannecarl@gmail.com>
2025-07-07 21:57:22 +0800

    changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
   ];
 
   return (
-    <div className="w-full h-auto py-3 px-4 md:px-[50px] lg:px-[200px] flex items-center text-[#EEEEEE] justify-between bg-red-900">
+    <div className="w-full h-auto py-3 px-4 md:px-[50px] lg:px-[200px] flex items-center text-[#EEEEEE] justify-between">
       <div className="flex gap-3 items-center">
         <p className="sm:text-[15px] text-[13px]">bulilaniannecarl@Gmail.com</p>
         <button className="bg-[#DCC7A1] relative flex justify-center h-[34px] w-[40px] rounded-md font-bold text-[14px] text-background sm:hover:w-[60px] overflow-hidden transition-all duration-300 ease-[cubic-bezier(0.76,0,0.24,1)] group">
@@ -52,7 +52,7 @@ export default function Navbar() {
               {item.title}
             </span>
 
-            <span className="absolute left-0 top-0 pl-2 translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-[cubic-bezier(0.76,0,0.24,1)] text-[#DCC7A1]">
+            <span className="absolute left-0 top-0 pl-1 translate-x-full group-hover:translate-x-0 transition-transform duration-300 ease-[cubic-bezier(0.76,0,0.24,1)] text-[#DCC7A1]">
               {item.title}
             </span>
 
